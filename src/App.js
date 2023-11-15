9c84ac149f2419172a60f810a7530d31200197f2
Nelson Palacios <nrpc10@gmail.com>
2023-11-15 12:06:34 -0600

    create hacker stories app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const List = (props) => (
+    <ul>
+    {props.list.map((item) => 
+    <Item key={item.objectID} item={item} />
+   )};
+    </ul>
+);
+
+const Item = (props) => (
+
+  <li>
+    <h2>{props.item.title}</h2>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+  );
+ 
+  const Search = (props) => {
+    const [searchTerm, setSearchTerm] = React.useState(' ');
+
+    const handleChange = (e) => {
+      setSearchTerm(e.target.value);
+
+      // C
+      props.onSearch(e);
+    };
+
+    return (
+        <div>
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text" onChange={props.onChange} />
+          <button type='submit'>Add</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Searching for <strong>{searchTerm}</strong>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        </div>
+      );
+  };
+     
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+  // A
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    console.log(e.target.value);
+  };
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+ 
+      {/* // B */}
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+     <List list={stories} />
+
     </div>
   );
-}
+};
 
 export default App;
